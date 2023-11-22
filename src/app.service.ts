895da43d52cdb4f061a58e86a1fae91586ea317b
Tadesse Alemayehu <tadessedev@gmail.com>
2023-11-22 16:17:53 +0300

    Emit event to add numbers

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class AppService {
+  constructor(@Inject('MATH_SERVICE') private readonly client: ClientProxy) {}
   getHello(): string {
+    console.log("emitting service")
+    this.client.emit<number>('add', [1, 2]);
     return 'Hello World!';
   }
 }
